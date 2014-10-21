7a41ec066dbea3c6f5b71f76e0c79e12d235b6f9
timmik94 <timmik94@mail.ru>
2014-10-21 17:42:41 +0400

    using navmesh

---
+++ Assets/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StudentController : MonoBehaviour {
+    public int auditory;
+    private NavMeshAgent nma;
+	// Use this for initialization
+	void Start () {
+        nma = gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject[] auditories = GameObject.FindGameObjectsWithTag("Auditory");
+        Vector3 target = Vector3.zero;
+        for (int i = 0; i < auditories.Length; i++)
+        {
+            if (auditory == auditories[i].GetComponent<Auditory>().number) { target = auditories[i].transform.position; break; }
+        }
+        nma.SetDestination(target);
+	
+	}
+}
